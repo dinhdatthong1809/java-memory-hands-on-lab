c8bfc1e9ff158ee4e71b02864a5526a58372740a
tdd <tdd@elca.vn>
2020-08-22 18:20:26 +0700

    h

--- src/main/java/vn/elca/training/memoryhandsonlab/MemoryHandsOnLabApplication.java
+++ src/main/java/vn/elca/training/memoryhandsonlab/MemoryHandsOnLabApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MemoryHandsOnLabApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppConfig.class, args);
-	}
-	
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+    
 }
